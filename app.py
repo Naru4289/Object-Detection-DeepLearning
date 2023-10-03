a06c1100c82257d97f5ee54932392612f21fd8bf
Murali Krishna Asoori <118078324+Naru4289@users.noreply.github.com>
2023-10-03 19:15:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import cv2
+import supervision as sv
+from ultralytics import YOLO
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+# define resolution
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+# specify the model
+model = YOLO("yolov8n.pt")
+
+# customize the bounding box
+box_annotator = sv.BoxAnnotator(
+    thickness=2,
+    text_thickness=2,
+    text_scale=1
+)
+
+def generate_frames():
+    while True:
+        ret, frame = cap.read()
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_yolov8(result)
+        labels = [
+            f"{model.model.names[class_id]} {confidence:0.2f}"
+            for _, confidence, class_id, _
+            in detections
+        ]
+        frame = box_annotator.annotate(
+            scene=frame, 
+            detections=detections, 
+            labels=labels
+        ) 
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
